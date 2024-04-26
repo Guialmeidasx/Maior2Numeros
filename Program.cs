1fa53a28537a32237d4cb53f60b68cf515d72d08
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-04-26 20:30:11 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿//Digite o primeiro número..: 7
+//Digite o segundo número...: 15
+//O maior número é 15
+
+int Primeiro, Segundo;
+
+Console.Write("Digite o primeiro número..:");
+Primeiro = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite o segundo número..:");
+Segundo = Convert.ToInt32(Console.ReadLine());
+
+if (Primeiro>Segundo){
+        Console.Write($"O maior número é..: {Primeiro}");
+}
+
+else {
+        Console.Write($"O maior número é..: {Segundo}");
+}
+
